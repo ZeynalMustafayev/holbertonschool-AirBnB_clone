2b3e9b75feab04faedebbfd168e6f89011630dad
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-02-26 08:35:36 +0000

    class BaseModel until to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""unittest"""
 
 class BaseModel:
+    """BaseModel class"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = date_time.now()
+        self.updated_at = date_time.now()
+
+    def __str__(self):
+        print("[BaseClass] ({}) {}".format(self.id, self.__dict__))
+
+    def save(self):
+        updated_at = date_time.now()
+
+    def to_dict(self):
 

f3b166e7294c51a069e3a2599d5e7bc7200a755a
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-27 13:48:31 +0400

    base model fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,30 @@
 #!/usr/bin/python3
-"""unittest"""
+"""Base model"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel class"""
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != __class__:
-                    if key == created_at or key == updated_at:
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    def __str__(self):
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
+        """Saving datas"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
+        """Convert class infos to dict"""
+        new = {
+            "my_number": self.__dict__.get("my_number"),
+            "name": self.__dict__.get("name"),
+            "__class__": self.__class__.__name__,
+            "updated_at": self.updated_at.isoformat(),
+            "id": self.id,
+            "created_at": self.created_at.isoformat()
+        }
+        return new
 
-
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-print("--")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
-
-print("--")
-print(my_model is my_new_model)
+    def __str__(self) -> str:
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"

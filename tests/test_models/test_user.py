828783f64080c5268b93841aadc971e090c4dca7
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-28 14:24:37 +0400

    test user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,33 @@
+import unittest
+
+from datetime import datetime
+
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.model = User()
+        self.model.created_at = datetime.now()
+        self.model.updated_at = datetime.now()
+
+    def test_id(self):
+        self.model_test = User()
+        self.assertNotEqual(self.model.id, self.model_test.id)
+
+    def test_save(self):
+        old_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(old_updated_at, self.model.updated_at)
+        with open("file.json", "r") as file:
+            self.assertIn("User.{}".format(self.model.id), file.read())
+
+    def test_to_dict(self):
+        result = self.model.to_dict()
+        self.assertEqual(result["__class__"], self.model.__class__.__name__)
+        self.assertEqual(result["created_at"], self.model.created_at.isoformat())
+        self.assertEqual(result["updated_at"], self.model.updated_at.isoformat())
+
+    def test_str(self):
+        expected_str = "[{}] ({}) {}".format(self.model.__class__.__name__, self.model.id, self.model.__dict__)
+        self.assertEqual(str(self.model), expected_str)

0e2512cbda166486b8c90226b483c51a24d7a6b2
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-02-28 10:53:26 +0000

    fix float problem

--- models/place.py
+++ models/place.py
@@ -10,8 +10,8 @@ class Place(BaseModel):
     number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
-    latitude = float(0)
-    longtitude = float(0)
+    latitude = 0.0
+    longtitude = 0.0
     amenity_ids = []
 
     def __init__(self, *args, **kwargs):
